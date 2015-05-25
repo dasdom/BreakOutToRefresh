341bbbe94f80356d341dbab67e5fdcec587d785b
Rachel Brindle <you@subluminal.net>
2015-05-24 20:04:47 -0700

    Create BreakOutToRefresh dynamic library target/scheme

---
+++ PullToRefresh/BreakOutToRefresh.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for PullToRefresh.
+FOUNDATION_EXPORT double PullToRefreshVersionNumber;
+
+//! Project version string for PullToRefresh.
+FOUNDATION_EXPORT const unsigned char PullToRefreshVersionString[];
+
